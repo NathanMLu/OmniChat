706bf1e15637cda5d034a49c33cb5813f9c53bb5
Jamey Bryce <jybryce4@gmail.com>
2022-02-05 09:40:03 -0600

    Began work on main content - Feed component

---
+++ ClientApp/src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <main>
+        <div className='message'>
+            <p>Message Text</p>
+        </div>
+    </main>
+  );
+};
+
+export default Feed;
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import Feed from './Feed';
 import Footer from './Footer';
 
-
-
 const Home = () => {
-  return <div>
-    <Header />
-    <Footer />
-  </div>;
+  return (
+    <div>
+        <Header />
+        <Feed />
+        <Footer />
+    </div>
+  );
 }
 
 export default Home;
