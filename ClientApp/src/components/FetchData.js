4725c2fa4f1c5f7f98016dc2fa15e1f476509f54
Nathan Lu <nathanlumax@gmail.com>
2022-02-05 20:33:16 -0600

    Got proxy working finallyyyyy

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,4 +1,3 @@
-/*
 import React, {Component} from 'react';
 
 export class FetchData extends Component {
@@ -6,65 +5,21 @@ export class FetchData extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {forecasts: [], loading: true};
+        this.state = {messages: [], loading: true};
     }
 
     componentDidMount() {
-        this.populateMessages();
+        this.populateMessage();
     }
 
-    static renderMessages(messages) {
-        return (
-            <div>
-                <h1>test</h1>
-            </div>
-        );
-    }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : FetchData.renderMessages(this.state.forecasts);
-
-        return (
-            <div>
-                <h1>Testing Message Data :(</h1>
-                <p>Testing if I can get message data</p>
-                {contents}
-            </div>
-        );
-    }
-
-    async populateMessages() {
-        const response = await fetch('user');
-        const data = await response.json();
-        this.setState({forecasts: data, loading: false});
-    }
-}
-*/
-
-import React, {Component} from 'react';
-
-export class FetchData extends Component {
-    static displayName = FetchData.name;
-
-    constructor(props) {
-        super(props);
-        this.state = {forecasts: [], loading: true};
-    }
-
-    componentDidMount() {
-        this.populateMessages();
-    }
-
-    static renderMessages(forecasts) {
+    static renderMessage(message) {
         return (
             <div>
                 <h1>test</h1>
 
                 <p>
-                    {forecasts.name}
-                    {forecasts.platform}
+                    {message.name}
+                    {message.platform}
                 </p>
             </div>
         );
@@ -73,25 +28,21 @@ export class FetchData extends Component {
     render() {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : FetchData.renderMessages(this.state.forecasts);
+            : FetchData.renderMessage(this.state.messages);
 
         return (
             <div>
                 <h1>Testing Message Data</h1>
-                <p>This component demonstrates fetching data from the server.</p>
+                <p>trying to get data from c# backend.</p>
                 {contents}
             </div>
         );
     }
 
-    async populateMessages() {
+    async populateMessage() {
         const response = await fetch('user');
-            // .then(response => response.json())
-            // .then(data => {
-            //     console.log(data.date);
-            // });
         const data = await response.json();
         console.log(data);
-        this.setState({forecasts: data, loading: false});
+        this.setState({messages: data, loading: false});
     }
 }
