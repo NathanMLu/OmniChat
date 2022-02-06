a5f16cc4d6073ae36cb98c05aebc1d62a8a16132
Jamey Bryce <jybryce4@gmail.com>
2022-02-05 21:05:50 -0600

    got front-end connected to back and started formatting the UI

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -17,8 +17,8 @@ export class Home extends Component {
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">{message.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted ">from {message.platform} at {message.messageReceived.LatestTimestamp}</h6>
-                <p className="card-text">{message.messageReceived.LatestMessage}</p>
+                <h6 className="card-subtitle mb-2 text-muted ">from {message.platform} at {message.messageReceived.latestTimestamp}</h6>
+                <p className="card-text">{message.messageReceived.latestMessage}</p>
               </div>
             </div>
         );
@@ -27,7 +27,7 @@ export class Home extends Component {
     render() {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : Home.renderMessage(this.state.messages);
+            : this.state.messages.map(message => Home.renderMessage(message));
 
         return (
             <div>
@@ -39,9 +39,11 @@ export class Home extends Component {
     }
 
     async populateMessage() {
+        const messagesArr = [];
         const response = await fetch('user');
-        const data = await response.json();
-        this.setState({messages: data, loading: false});
+        messagesArr.push(await response.json());
+        console.log(messagesArr)
+        this.setState({messages: messagesArr, loading: false});
     }
 }
 
