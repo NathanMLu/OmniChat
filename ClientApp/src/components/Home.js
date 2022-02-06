0f1384e51ff02b09cf833125bf9e77b68677808f
Jamey Bryce <jybryce4@gmail.com>
2022-02-05 23:03:41 -0600

    Message component made

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -17,13 +17,7 @@ export class Home extends Component {
     static renderMessage(message) {
         return (
             <>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{message.name}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted ">from {message.platform} at {message.messageReceived.latestTimestamp}</h6>
-                    <p className='card-text'>{message.messageReceived.latestMessage}</p>
-                </div>
-            </div>
+                <Message key={message.id} message={message} />
             </>
         );
     }
@@ -45,11 +39,9 @@ export class Home extends Component {
     }
 
     async populateMessage() {
-        const messagesArr = [];
         const response = await fetch('user');
-        messagesArr.push(await response.json());
-        console.log(messagesArr);
-        this.setState({messages: messagesArr, loading: false});
+        const data = await response.json();
+        this.setState({messages: data, loading: false});
     }
 }
 
--- ClientApp/src/components/Message.js
+++ ClientApp/src/components/Message.js
@@ -4,7 +4,13 @@ import './Sidebar.css';
 
 function Message({ message }) {
   return (
-    <p>{message.messageReceived.latestMessage}</p>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{message.name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted ">from {message.platform} at {message.messageReceived.latestTimestamp}</h6>
+        <p className='card-text'>{message.messageReceived.latestMessage}</p>
+      </div>
+    </div>
   );
 };
 
